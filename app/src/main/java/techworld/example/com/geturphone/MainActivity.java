7194402170eb37ada3db9d9fe523b838db180dbb
Vipul Garg <vipulgarg95@gmail.com>
2015-06-24 00:38:51 +0800

    commit 24-06-15 12:38 am

--- app/src/main/java/techworld/example/com/geturphone/MainActivity.java
+++ app/src/main/java/techworld/example/com/geturphone/MainActivity.java
@@ -2,16 +2,17 @@ package techworld.example.com.geturphone;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_main);
     }
 
     @Override
