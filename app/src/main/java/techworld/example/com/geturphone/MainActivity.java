5ea3a59cfd2028038cfbba859fff17804f97b232
Vipul Garg <vipulgarg95@gmail.com>
2015-06-24 00:52:48 +0800

    commit 24-06-15 12:38 am

--- app/src/main/java/techworld/example/com/geturphone/MainActivity.java
+++ app/src/main/java/techworld/example/com/geturphone/MainActivity.java
@@ -1,6 +1,4 @@
 package techworld.example.com.geturphone;
-
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
